c39cdb91525a83be08ffa91b1e1917f8fc5d66b8
dev_krissh-at-629937844728 <krishnamitws2000@gmail.com>
2023-05-30 13:21:06 +0530

    ajv error'

--- src/Components/AddExpenseForm.js
+++ src/Components/AddExpenseForm.js
@@ -29,31 +29,31 @@ const AddExpenseForm = (props) => {
         <form onSubmit={onSubmit}>
             <div className='row'>
                 <div className='col-sm col-lg-4'>
-                    <label for='name'>Name</label>
+                    <label htmlFor='name'>Name</label>
                     <input
                         required='required'
                         type='text'
-                        class='form-control'
+                        className='form-control'
                         id='name'
                         value={name}
                         onChange={(event) => setName(event.target.value)}
                     />
                 </div>
-                <div class='col-sm col-lg-4'>
-                    <label for='cost'>Cost</label>
+                <div className='col-sm col-lg-4'>
+                    <label htmlFor='cost'>Cost</label>
                     <input
                         required='required'
                         type='number'
-                        class='form-control'
+                        className='form-control'
                         id='cost'
                         value={cost}
                         onChange={(event) => setCost(event.target.value)}
                     />
                 </div>
             </div>
-            <div class='row mt-3'>
-                <div class='col-sm'>
-                    <button type='submit' class='btn btn-primary'>
+            <div className='row mt-3'>
+                <div className='col-sm'>
+                    <button type='submit' className='btn btn-primary'>
                         Save
                     </button>
                 </div>
